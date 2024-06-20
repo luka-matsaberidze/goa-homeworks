f7d11dfccd0661491a4d43fde6decab20194df32
luka-matsaberidze <l.ukamatsaberidze333@gmail.com>
2024-06-20 22:39:40 +0400

    second commit

--- DAY 1/day1.py
+++ DAY 1/day1.py
@@ -1,2 +1,50 @@
-print("luka matsaberidze")
-print("this is day 1")
+print ("luka matsaberidze") 
+
+print ("this is day 1")
+
+name = "luka"
+
+surname = "matsaberidze"
+
+
+# name aris cvladi
+
+# "=" aris cvlais mnisvnelobis mimnijbeli simbolo
+
+# "nika" aris cvladisvis mnisvneloba
+
+print (name)
+ 
+name = "luka"      #es aris str (string) tipis cvladi
+
+age = 14          #es aris int (integer) mteli ricxvi
+
+height = 179     #es aris "floaf" tipis cvladi (atwiladi)
+
+#boolean (bool) tipis cvladi
+ 
+good_at_math = True        # "true" an "false"
+
+is_ugly = False   #snakecase  (weris stili)
+
+isUgly = False
+
+
+print (name + " " + surname)
+
+
+print (type(age))     #"int"
+print (type(name))     #"str"
+print (type(surname))    #"str"
+print (type(height))      #"int"
+print (type(good_at_math))    #"bool"
+
+
+
+
+print (name + " " + surname + " " +  "aris" " " + str(age) + " " + "wlis, igi aris"+ " " + str(height) + " " + "metris" + " " + "is aris kargi matematiksi"  )
+ 
+
+
+
+
