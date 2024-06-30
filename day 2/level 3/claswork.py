a6d0b08ec5d16504b03b904de5f28aced6231b91
luka-matsaberidze <l.ukamatsaberidze333@gmail.com>
2024-07-01 02:28:48 +0400

    second commit

---
+++ day 2/level 3/claswork.py
@@ -0,0 +1,12 @@
+print("goa best")
+
+
+library = "book"
+name = "luka"
+car = "dmw"
+
+#cvladi
+
+print(library)
+print(name)
+print(car)
---
+++ day 2/level 3/homework.py
@@ -0,0 +1,17 @@
+
+
+age = "14 "
+
+name = "luka "
+
+nickname = "macaberidze "
+
+live_in = "rustavsi"
+
+sport  = "footballs"
+
+he_has = "bevri megobari"
+
+print(name +  nickname +   "aris " + str(age) +   "wlis," + " " + "is cxovrobs " + live_in + ", " + "is tamasobs" + " " + sport + ", " + "mas yavs"+  " " + he_has + " " + live_in )
+
+c:\Users\TECHNOPC\Pictures\download (3).jfif
